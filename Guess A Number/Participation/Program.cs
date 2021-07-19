70a38f8294021dc9474d3ca4cc5f26741795231b
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-07-19 13:27:51 -0500

    Finished the Participation

---
+++ Guess A Number/Participation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user, please input your min value. >>");
+            string min = Console.ReadLine();
+
+            Console.WriteLine("Please input your max value. >>");
+            string max = Console.ReadLine();
+
+            int minValue;
+            int maxValue;
+
+            if (int.TryParse(min, out minValue) != true)
+            {
+                Console.WriteLine("Please input a number.");
+                Environment.Exit(1);
+            }
+
+            if (int.TryParse(max, out maxValue) != true)
+            {
+                Console.WriteLine("Please input a valid number.");
+                Environment.Exit(1);
+            }
+
+
+            int number;
+            int guess;
+            do
+            {
+
+            Random rand = new Random();
+            number = rand.Next(minValue, maxValue);
+            
+            Console.WriteLine("Now, take a guess between your minimum and your maximum. >>");
+            guess = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"You guessed ({guess}) and the random number is ({number}).");
+
+            if (guess == number)
+            {
+                Console.WriteLine("Congratulations, you got the correct number!");
+            }
+            else
+            {
+                Console.WriteLine("You got it wrong! Try again!");
+            }
+            } while (guess != number);
+        }
+    }
+}
